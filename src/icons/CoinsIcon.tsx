1aa7af141197c873deb705d9b0146f1e15a44a40
alojzy231 <alojzy231@gmail.com>
2022-06-01 16:45:21 +0200

    Added a heart and coins icons

---
+++ src/icons/CoinsIcon.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import PropTypes, { InferType } from 'prop-types';
+
+export default function CoinsIcon({
+  className,
+}: InferType<typeof CoinsIcon.propTypes>): JSX.Element {
+  return (
+    <svg
+      className={className}
+      width="32"
+      height="31"
+      viewBox="0 0 32 31"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_8_48)">
+        <circle cx="14" cy="16" r="7" fill="#FAFFDF" />
+        <circle cx="14" cy="16" r="6.5" stroke="black" />
+      </g>
+      <g filter="url(#filter1_d_8_48)">
+        <circle cx="19" cy="12" r="8" fill="#FAFFDF" />
+        <circle cx="19" cy="12" r="7.5" stroke="black" />
+      </g>
+      <path
+        d="M19.6074 5.43359V7.51855H18.582V5.43359H19.6074ZM19.4844 16.5967V18.4219H18.4658V16.5967H19.4844ZM20.8516 14.4365C20.8516 14.1631 20.79 13.917 20.667 13.6982C20.5439 13.4795 20.3411 13.279 20.0586 13.0967C19.776 12.9144 19.3978 12.7458 18.9238 12.5908C18.3496 12.4131 17.8529 12.1966 17.4336 11.9414C17.0189 11.6862 16.6976 11.3695 16.4697 10.9912C16.2464 10.613 16.1348 10.1549 16.1348 9.61719C16.1348 9.05664 16.2555 8.57357 16.4971 8.16797C16.7386 7.76237 17.0804 7.4502 17.5225 7.23145C17.9645 7.0127 18.484 6.90332 19.0811 6.90332C19.5459 6.90332 19.9606 6.97396 20.3252 7.11523C20.6898 7.25195 20.9974 7.45703 21.248 7.73047C21.5033 8.00391 21.6969 8.33887 21.8291 8.73535C21.9658 9.13184 22.0342 9.58984 22.0342 10.1094H20.7764C20.7764 9.80404 20.7399 9.52376 20.667 9.26855C20.5941 9.01335 20.4847 8.79232 20.3389 8.60547C20.193 8.41406 20.0153 8.26823 19.8057 8.16797C19.596 8.06315 19.3545 8.01074 19.0811 8.01074C18.6982 8.01074 18.3815 8.07682 18.1309 8.20898C17.8848 8.34115 17.7025 8.52799 17.584 8.76953C17.4655 9.00651 17.4062 9.28223 17.4062 9.59668C17.4062 9.88835 17.4655 10.1436 17.584 10.3623C17.7025 10.5811 17.903 10.7793 18.1855 10.957C18.4727 11.1302 18.8669 11.3011 19.3682 11.4697C19.9561 11.6566 20.4551 11.8776 20.8652 12.1328C21.2754 12.3835 21.5876 12.6934 21.8018 13.0625C22.016 13.4271 22.123 13.8805 22.123 14.4229C22.123 15.0107 21.9909 15.5075 21.7266 15.9131C21.4622 16.3141 21.0908 16.6195 20.6123 16.8291C20.1338 17.0387 19.5732 17.1436 18.9307 17.1436C18.5433 17.1436 18.1605 17.0911 17.7822 16.9863C17.404 16.8815 17.0622 16.7106 16.7568 16.4736C16.4515 16.2321 16.2077 15.9154 16.0254 15.5234C15.8431 15.127 15.752 14.6416 15.752 14.0674H17.0234C17.0234 14.4548 17.0781 14.776 17.1875 15.0312C17.3014 15.2819 17.4518 15.4824 17.6387 15.6328C17.8255 15.7786 18.0306 15.8835 18.2539 15.9473C18.4818 16.0065 18.7074 16.0361 18.9307 16.0361C19.3408 16.0361 19.6872 15.9723 19.9697 15.8447C20.2568 15.7126 20.4756 15.5257 20.626 15.2842C20.7764 15.0426 20.8516 14.7601 20.8516 14.4365Z"
+        fill="black"
+      />
+      <defs>
+        <filter
+          id="filter0_d_8_48"
+          x="0"
+          y="5"
+          width="26"
+          height="26"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="-1" dy="2" />
+          <feGaussianBlur stdDeviation="3" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.8 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_8_48" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_8_48" result="shape" />
+        </filter>
+        <filter
+          id="filter1_d_8_48"
+          x="4"
+          y="0"
+          width="28"
+          height="28"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="-1" dy="2" />
+          <feGaussianBlur stdDeviation="3" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.8 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_8_48" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_8_48" result="shape" />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
+
+CoinsIcon.propTypes = {
+  className: PropTypes.string,
+};
+
+CoinsIcon.defaultProps = {
+  className: '',
+};
